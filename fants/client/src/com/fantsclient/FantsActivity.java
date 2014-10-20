28ec8849f7c94b94864e57096f668231e0c73664
Astra <Astra@Astra-PC.Speedport_W723_V_Typ_A_1_01_009>
2014-10-20 20:02:16 +0200

    Stand 20.10.2014

---
+++ fants/client/src/com/fantsclient/FantsActivity.java
@@ -0,0 +1,15 @@
+package com.fantsclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FantsActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fants);
+
+	};
+
+}
